ebb13d6e6c59afc046ea010b55e9a21ded572a11
Dan Mohr <danmohr@live.com>
2016-03-15 19:47:23 -0400

    updated graphs.... want to embed these in the page... merge with branch from Utilant

---
+++ DEV/assets/js/graphs.js
@@ -0,0 +1,20 @@
+/*
+	Halcyonic by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	debugger; 
+	document.getElementById('test').onclick = TestClickHandler;
+
+
+})(jQuery);
+
+function TestClickHandler() {
+	alert('winning');
+	// redirect to the graphs here...
+	window.location = "Graphs/Dashboard.html";
+
+}
