c87a16c2329117b06ee5efed3e829bde7f66caf8
Dan Mohr <dmohr@utilant.com>
2016-09-28 12:50:18 -0400

    wip

---
+++ DEV/OLD/Copy to IIS.bat
@@ -0,0 +1,2 @@
+robocopy "C:\Users\dmohr\Documents\Personal\Uncle Jack Website\DEV" "C:\Users\dmohr\Documents\Personal\StrayLightopticsHosted" *.html *csv
+pause
---
+++ DEV/OLD/assets/js/PSD.js
@@ -0,0 +1,20 @@
+// https://maurizzzio.github.io/function-plot/
+
+
+functionPlot({
+  title: 'y = x * x',
+  target: '#quadratic-with-options',
+  width: 580,
+  height: 400,
+  disableZoom: true,
+  xAxis: {
+    label: 'x - axis',
+    domain: [-6, 6]
+  },
+  yAxis: {
+    label: 'y - axis'
+  },
+  data: [{
+    fn: 'x^2'
+  }]
+})
